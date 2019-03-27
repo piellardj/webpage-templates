004da61cb36d48b6062af10ce2eb89dffb986cc2
Jérémie PIELLARD <jeremie.piellard@gmail.com>
2019-03-27 21:06:50 +0100

    Each error message now has a unique ID and can be set/deleted individually

--- build/components/demopage/main/handler.js
+++ build/components/demopage/main/handler.js
@@ -6,16 +6,48 @@ const Demopage = (function() {
         console.error("Cannot find element '" + errorsBlockId + "'.");
     }
 
+    /**
+     * @param {string} id
+     * @return {object}
+     */
+    function getErrorById(id) {
+        return errorsBlock.querySelector("span[id=error-message-" + id +"]");
+    }
+
     return Object.freeze({
         /**
+         * @param {string} id
          * @param {string} message
          */
-        addErrorMessage: function(message) {
+        setErrorMessage: function(id, message) {
             if (errorsBlock) {
-                const span = document.createElement("span");
-                span.innerText = message;
-                errorsBlock.appendChild(span);
-                errorsBlock.appendChild(document.createElement("br"));
+                const span = getErrorById(id);
+                if (span) {
+                    span.innerHTML = message;
+                    return;
+                } else {
+                    const span = document.createElement("span");
+                    span.id = "error-message-" + id;
+                    span.innerText = message;
+                    errorsBlock.appendChild(span);
+                    errorsBlock.appendChild(document.createElement("br"));
+                }
+            }
+        },
+
+        /**
+         * @param {string} id
+         */
+        removeErrorMessage: function(id) {
+            if (errorsBlock) {
+                const span = getErrorById(id);
+                if (span) {
+                    const br = span.nextElementSibling;
+                    if (br) {
+                        errorsBlock.removeChild(br);
+                    }
+                    errorsBlock.removeChild(span);
+                }
             }
         },
     });
