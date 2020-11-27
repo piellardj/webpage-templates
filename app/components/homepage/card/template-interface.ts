c63888db910572915e95a580cce04912cc49835e
Jérémie Piellard <jeremie.piellard@gmail.com>
2020-11-27 21:18:14 +0100

    Homepage: added support for multiple images for different device dpi

--- app/components/homepage/card/template-interface.ts
+++ app/components/homepage/card/template-interface.ts
@@ -1,6 +1,8 @@
 export interface ICard {
-    /* path to the background image */
-    background: string;
+    // background images must have an aspect ratio of 2:1, and will be displayed with a height of 256px
+    background: string; // must be a format supported everywhere (png or jpg)
+    background_light?: string; // can be any format (webp etc.)
+    background_light_highdpi?: string; // can be any format (webp etc.)
 
     title: string;
     projectName: string;
--- build/components/homepage/card/template-interface.d.ts
+++ build/components/homepage/card/template-interface.d.ts
@@ -1,5 +1,7 @@
 export interface ICard {
     background: string;
+    background_light?: string;
+    background_light_highdpi?: string;
     title: string;
     projectName: string;
     githubLink?: boolean;
